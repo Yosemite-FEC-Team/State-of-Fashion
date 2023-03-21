82f3944a2096b04aa5b19c49aee4bf69051a2fcc
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-03-21 17:24:15 -0400

    Resizing of expanded view images and buttons

--- src/overview/Expanded.jsx
+++ src/overview/Expanded.jsx
@@ -17,15 +17,15 @@ const Expanded = ({revert, galleryList, mainImage, setMainImage}) => {
     let imageID = `slide${index}`;
     return (
       <div id={imageID} className={ index === mainImage ? 'expanded-card' : 'inactive-card'}>
-          <button className="right-arrow btn btn-ghost" onClick={nextSlide}>{mainImage === galleryList.length - 1 ? '' : '❯'}</button>
-          <button className="left-arrow btn btn-ghost" onClick={prevSlide}>{mainImage === 0 ? '' : '❮'}</button>
+          {mainImage !== galleryList.length ? <button className="right-arrow btn btn-accent" onClick={nextSlide}>❯</button> : ''}
+          {mainImage !== 0 ? <button className="left-arrow btn btn-accent" onClick={prevSlide}>❮</button> : ''}
           {index === mainImage && (<img className='expanded-image' src={image.url}></img>)}
       </div>)
   })
 
   return(
-    <div className='expanded-view'>
-      <button className='expanded-button btn btn-square btn-outline btn-sm' onClick={revert}>X</button>
+    <div className='expanded-view w-full'>
+      <button className='expanded-button btn btn-square btn-outline btn-sm' onClick={revert}>x</button>
       {expandedCarousel}
     </div>
   )
