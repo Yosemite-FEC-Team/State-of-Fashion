da9ed16db4c237573a1269aff5ac762016d44f40
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-03-22 16:17:53 -0400

    Beginning implementations of expanded/zoom view

--- src/overview/Expanded.jsx
+++ src/overview/Expanded.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 const Expanded = ({revert, galleryList, mainImage, setMainImage}) => {
 
+  const [zoom, setZoom] = React.useState(1);
+
+  const handleZoomClick = () => {
+    setZoom(2.5);
+  }
+
   let expandedCarousel = galleryList.map(image => {
     let index = galleryList.indexOf(image);
 
@@ -14,12 +20,14 @@ const Expanded = ({revert, galleryList, mainImage, setMainImage}) => {
     }
 
 
+
     let imageID = `slide${index}`;
+    let imageStyle = { height: '1000px', transform: `scale(${zoom})` }
     return (
       <div id={imageID} className={ index === mainImage ? 'expanded-card' : 'inactive-card'}>
           {mainImage !== galleryList.length - 1 ? <button className="right-arrow btn btn-accent" onClick={nextSlide}>❯</button> : ''}
           {mainImage !== 0 ? <button className="left-arrow btn btn-accent" onClick={prevSlide}>❮</button> : ''}
-          {index === mainImage && (<img className='expanded-image' src={image.url}></img>)}
+          {index === mainImage && (<img className='expanded-image w-full' onClick={handleZoomClick} style={imageStyle} src={image.url}></img>)}
       </div>)
   })
 
