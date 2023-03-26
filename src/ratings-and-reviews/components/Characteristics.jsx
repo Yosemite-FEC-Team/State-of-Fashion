438a5287583bb825473982eda69003a39448004a
Lizz Mullowney <elizabethmullowney@gmail.com>
2023-03-26 10:58:47 -0500

    save css changes

--- src/ratings-and-reviews/components/Characteristics.jsx
+++ src/ratings-and-reviews/components/Characteristics.jsx
@@ -2,10 +2,16 @@ import React, {useEffect, useState} from 'react';
 
 const Characteristics = () => {
 
+  const trianglePercentage = 45;
   return (
     <div className='characteristics-bars'>
     <h5>Size</h5>
-    <h5>Size Bar</h5>
+    <div className="triangle-container">
+      <div className="rectangle" />
+      <div className="rectangle" />
+      <div className="right-rectangle" />
+      <div className="triangle" style={{ left: `${trianglePercentage}%` }} />
+    </div>
     <h5>Width</h5>
     <h5>Width Bar</h5>
     <h5>Comfort</h5>
--- src/ratings-and-reviews/components/Stars.jsx
+++ src/ratings-and-reviews/components/Stars.jsx
@@ -25,17 +25,56 @@ const Stars = ({data}) => {
 
   var bigRating = createAverageRating(data);
 
+  var arrayOfTotalRatings = Object.values(data);
+
+  var fiveStarPercentage = ((Number(arrayOfTotalRatings[4])/sum)*100).toFixed(0);
+  var fourStarPercentage = ((Number(arrayOfTotalRatings[3])/sum)*100).toFixed(0);
+  var threeStarPercentage = ((Number(arrayOfTotalRatings[2])/sum)*100).toFixed(0);
+  var twoStarPercentage = ((Number(arrayOfTotalRatings[1])/sum)*100).toFixed(0);
+  var oneStarPercentage = ((Number(arrayOfTotalRatings[0])/sum)*100).toFixed(0);
+
+  console.log(fiveStarPercentage, 'five star percentage');
   return (
     <div className='stars-component'>
     <span className='big-rating'>{bigRating}</span>
     <span> stars</span>
     <span className='small-text'>({sum} total reviews)</span>
     <h3>100% of reviews recommend this product</h3>
-    <span><small><u>5 stars</u></small></span>
-    <h3>4 stars BAR</h3>
-    <h3>3 stars BAR</h3>
-    <h3>2 stars BAR</h3>
-    <h3>1 star BAR</h3>
+    <div className='stars-and-bar-container'>
+      <div className='x-stars'><small><u>5 stars</u></small></div>
+      <div className='star-bar-container'>
+        <div className='star-bar-fill' style={{width: `${fiveStarPercentage}%`}} />
+        <div className="star-bar-unfill" style={{ width: `${100 - fiveStarPercentage}%` }} />
+      </div>
+    </div>
+    <div className='stars-and-bar-container'>
+      <div className='x-stars'><small><u>4 stars</u></small></div>
+      <div className='star-bar-container'>
+        <div className='star-bar-fill' style={{width: `${fourStarPercentage}%`}} />
+        <div className="star-bar-unfill" style={{ width: `${100 - fourStarPercentage}%` }} />
+      </div>
+    </div>
+    <div className='stars-and-bar-container'>
+      <div className='x-stars'><small><u>3 stars</u></small></div>
+      <div className='star-bar-container'>
+        <div className='star-bar-fill' style={{width: `${threeStarPercentage}%`}} />
+        <div className="star-bar-unfill" style={{ width: `${100 - threeStarPercentage}%` }} />
+      </div>
+    </div>
+    <div className='stars-and-bar-container'>
+      <div className='x-stars'><small><u>2 stars</u></small></div>
+      <div className='star-bar-container'>
+        <div className='star-bar-fill' style={{width: `${twoStarPercentage}%`}} />
+        <div className="star-bar-unfill" style={{ width: `${100 - twoStarPercentage}%` }} />
+      </div>
+    </div>
+    <div className='stars-and-bar-container'>
+      <div className='x-stars'><small><u>1 stars</u></small></div>
+      <div className='star-bar-container'>
+        <div className='star-bar-fill' style={{width: `${oneStarPercentage}%`}} />
+        <div className="star-bar-unfill" style={{ width: `${100 - oneStarPercentage}%` }} />
+      </div>
+    </div>
     </div>
   )
 }
